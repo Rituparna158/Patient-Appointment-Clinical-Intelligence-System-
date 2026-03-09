1fa30368f9f605b64561bd8bbb9df44a7e7333cc
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-03-09 03:45:10 -0700

    chore: relocate files to a new folder structure

---
+++ apps/web-app/src/features/dashboard/PatientDashboard.tsx
@@ -0,0 +1,19 @@
+import DashboardLayout from '@/app/layout/AppLayout';
+import { useAuthStore } from '@/store/auth/auth.store';
+
+export default function PatientDashboard() {
+  const user = useAuthStore((state) => state.user);
+
+  return (
+    <DashboardLayout>
+      <h1 className="page-heading">Patient Dashboard</h1>
+
+      <p className="page-subtext">Logged in as: {user?.email}</p>
+
+      <p className="page-description">
+        Book appointments, track visit history, view prescriptions, and access
+        medical records securely.
+      </p>
+    </DashboardLayout>
+  );
+}
