208ee4d9bbc85673fa62875aeb0d014c45cb1336
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-03-09 03:11:31 -0700

    chore: format code with prettier

--- apps/web-app/src/app/router/AppRoutes.tsx
+++ apps/web-app/src/app/router/AppRoutes.tsx
@@ -1,235 +1,233 @@
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import Home from "@/pages/home/Home";
-import Login from "@/pages/auth/Login";
-import Register from "@/pages/auth/Register";
+import Home from '@/features/home/Home';
+import Login from '@/features/auth/pages/Login';
+import Register from '@/features/auth/pages/Register';
 
-import PatientDashboard from "@/pages/dashboard/PatientDashboard";
-import DoctorDashboard from "@/pages/dashboard/DoctorDashboard";
-import AdminDashboard from "@/pages/dashboard/AdminDashboard";
+import PatientDashboard from '@/features/dashboard/PatientDashboard';
+import DoctorDashboard from '@/features/dashboard/DoctorDashboard';
+import AdminDashboard from '@/features/dashboard/AdminDashboard';
 
-import ProtectedRoute from "./ProtectedRoute";
-import ForgotPassword from "@/pages/auth/ForgotPassword";
-import ResetPassword from "@/pages/auth/ResetPassword";
-import Profile from "@/pages/profile/Profile";
-import CreateDoctor from "@/pages/admin/CreateDoctor";
-import CreateAdmin from "@/pages/admin/CreateAdmin";
+import ProtectedRoute from './ProtectedRoute';
+import ForgotPassword from '@/features/auth/pages/ForgotPassword';
+import ResetPassword from '@/features/auth/pages/ResetPassword';
+import Profile from '@/features/profile/page/Profile';
+import CreateDoctor from '@/features/admin/pages/CreateDoctor';
+import CreateAdmin from '@/features/admin/pages/CreateAdmin';
 
-import CreatePatientProfile from "@/pages/patient/CreatePatientProfile";
-import PatientProfilePage from "@/pages/patient/PatientProfile";
-import PatientListPage from "@/features/admin/patients/PatientListPage";
-import EditPatientProfile from "@/pages/patient/EditPatientProfile";
+import CreatePatientProfile from '@/features/patient/pages/CreatePatientProfile';
+import PatientProfilePage from '@/features/patient/pages/PatientProfile';
+import PatientListPage from '@/features/patient/pages/PatientListPage';
+import EditPatientProfile from '@/features/patient/pages/EditPatientProfile';
 
-import BookAppointment from "@/pages/patient/BookAppointment";
-import MyAppointments from "@/pages/patient/MyAppointments";
-import DoctorAppointments from "@/features/doctor/pages/DoctorAppointment";
-import CreateSlot from "@/features/admin/pages/CreateSlot";
-import CreateBranch from "@/features/admin/pages/CreateBranch";
-import AppointmentList from "@/features/admin/pages/AppointmentList";
+import BookAppointment from '@/features/patient/pages/BookAppointment';
+import MyAppointments from '@/features/patient/pages/MyAppointments';
+import DoctorAppointments from '@/features/doctor/pages/DoctorAppointment';
+import CreateSlot from '@/features/admin/pages/CreateSlot';
+import CreateBranch from '@/features/admin/pages/CreateBranch';
+import AppointmentList from '@/features/admin/pages/AppointmentList';
 
-import Payment from "@/pages/payment/Payment";
+import Payment from '@/features/payment/page/Payment';
 
-import DoctorConsultations from "@/features/doctor/pages/DoctorConsultations";
+import DoctorConsultations from '@/features/doctor/pages/DoctorConsultations';
 
-import PatientTimeline from "@/features/patient/pages/PatientTimeline";
-import AdminClinicalRecords from "@/features/admin/pages/AdminClinicalRecords";
+import PatientTimeline from '@/features/patient/pages/PatientTimeline';
+import AdminClinicalRecords from '@/features/admin/pages/AdminClinicalRecords';
 
-import AppLayout from "../layout/AppLayout";
+import AppLayout from '../layout/AppLayout';
+import DoctorPatientProfile from '@/features/clinical/pages/DoctorPatientProfile';
 
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
+      <Route
+        path="/patient/dashboard"
+        element={
+          <ProtectedRoute allowedRoles={['patient']}>
+            <PatientDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/doctor/dashboard"
+        element={
+          <ProtectedRoute allowedRoles={['doctor']}>
+            <DoctorDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/dashboard"
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
 
-export default function AppRoutes(){
-    return (
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
 
-            <Route path="/patient/dashboard"
-            element={
-                <ProtectedRoute  allowedRoles={["patient"]}>
-                    <PatientDashboard/>
-                </ProtectedRoute>
-            }
-            />
-            <Route path="/doctor/dashboard"
-            element={
-                <ProtectedRoute allowedRoles={["doctor"]}>
-                    <DoctorDashboard/>
-                </ProtectedRoute>
-            }
-            />
-            <Route path="/admin/dashboard"
-            element={
-                <ProtectedRoute  allowedRoles={["admin"]}>
-                    <AdminDashboard/>
-                </ProtectedRoute>
-            }
-            />
-            <Route path="/forgot-password" element={<ForgotPassword/>}/>
-            <Route path="/reset-password" element={<ResetPassword/>}/>
+      <Route
+        path="/admin/create-doctor"
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <CreateDoctor />
+          </ProtectedRoute>
+        }
+      />
 
-            <Route 
-            path="/profile"
-            element={
-                <ProtectedRoute>
-                    <Profile />
-                </ProtectedRoute>
-            }
-            />
+      <Route
+        path="/admin/create-admin"
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <CreateAdmin />
+          </ProtectedRoute>
+        }
+      />
 
-            <Route
-            path="/admin/create-doctor"
-             element={
-                <ProtectedRoute  allowedRoles={["admin"]}>
-                    <CreateDoctor/>
-                </ProtectedRoute>
-            }
-            />
+      <Route
+        path="/patient/create-profile"
+        element={
+          <ProtectedRoute allowedRoles={['patient']}>
+            <CreatePatientProfile />
+          </ProtectedRoute>
+        }
+      />
 
-            <Route
-            path="/admin/create-admin"
-             element={
-                <ProtectedRoute  allowedRoles={["admin"]}>
-                    <CreateAdmin/>
-                </ProtectedRoute>
-            }
-            />
+      <Route
+        path="/patient/profile"
+        element={
+          <ProtectedRoute allowedRoles={['patient']}>
+            <PatientProfilePage />
+          </ProtectedRoute>
+        }
+      />
 
-            <Route
-            path="/patient/create-profile"
-            element={
-                <ProtectedRoute allowedRoles={["patient"]}>
-                <CreatePatientProfile />
-                </ProtectedRoute>
-            }
-            />
+      <Route
+        path="/admin/patient"
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <AppLayout>
+              <PatientListPage />
+            </AppLayout>
+          </ProtectedRoute>
+        }
+      /> 
+      <Route
+        path="/patient/edit-profile"
+        element={
+          <ProtectedRoute allowedRoles={['patient']}>
+            <EditPatientProfile />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/patient/book-appointment"
+        element={
+          <ProtectedRoute allowedRoles={['patient']}>
+            <BookAppointment />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/patient/my-appointments"
+        element={
+          <ProtectedRoute allowedRoles={['patient']}>
+            <MyAppointments />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/doctor/appointments"
+        element={
+          <ProtectedRoute allowedRoles={['doctor']}>
+            <DoctorAppointments />
+          </ProtectedRoute>
+        }
+      />
+       <Route
+        path="/admin/appointments"
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+              <AppointmentList />
+          </ProtectedRoute>
+        }
+      /> 
+      <Route
+        path="/admin/create-slot"
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <AppLayout>
+              <CreateSlot />
+            </AppLayout>
+          </ProtectedRoute>
+        }
+      />
 
-            <Route
-            path="/patient/profile"
-            element={
-                <ProtectedRoute allowedRoles={["patient"]}>
-                        <PatientProfilePage />
-                </ProtectedRoute>
-            }
-            />
+      <Route
+        path="/admin/create-branch"
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <AppLayout>
+              <CreateBranch />
+            </AppLayout>
+          </ProtectedRoute>
+        }
+      />
 
-            <Route
-            path="/admin/patient"
-            element={
-                <ProtectedRoute allowedRoles={["admin"]}>
-                    <AppLayout>
-                        <PatientListPage />
-                    </AppLayout>
-                
-                </ProtectedRoute>
-            }
-            />
-            <Route
-            path="/patient/edit-profile"
-            element={
-                <ProtectedRoute allowedRoles={["patient"]}>
-                    <EditPatientProfile/>
-                </ProtectedRoute>
-            }
-            />
-            <Route
-            path="/patient/book-appointment"
-            element={
-                <ProtectedRoute allowedRoles={["patient"]}>
-                    <BookAppointment/>
-                </ProtectedRoute>
-            }
-            />
-            <Route
-            path="/patient/my-appointments"
-            element={
-                <ProtectedRoute allowedRoles={["patient"]}>
-                    <MyAppointments />
-                </ProtectedRoute>
-            }
-            />
-            <Route
-            path="/doctor/appointments"
-            element={
-                <ProtectedRoute allowedRoles={["doctor"]}>
-                    <DoctorAppointments />
-                </ProtectedRoute>
-            }
-            />
-            <Route
-            path="/admin/appointments"
-            element={
-                <ProtectedRoute allowedRoles={["admin"]}>
-                    <AppLayout>
-                        <AppointmentList />
-                    </AppLayout>
-                    
-                </ProtectedRoute>
-            }
-            />
-            <Route
-            path="/admin/create-slot"
-            element={
-                <ProtectedRoute allowedRoles={["admin"]}>
-                    <AppLayout>
-                        <CreateSlot />
-                    </AppLayout>
-                    
-                </ProtectedRoute>
-            }
-            />
+      <Route
+        path="/patient/payment/:appointmentId"
+        element={
+          <ProtectedRoute allowedRoles={['patient']}>
+            <Payment />
+          </ProtectedRoute>
+        }
+      />
 
-            <Route
-            path="/admin/create-branch"
-            element={
-                <ProtectedRoute allowedRoles={["admin"]}>
-                    <AppLayout>
-                        <CreateBranch />
-                    </AppLayout>
-                    
-                </ProtectedRoute>
-            }
-            />
+       <Route
+        path="/doctor/consultations"
+        element={
+          <ProtectedRoute allowedRoles={['doctor']}>
+            <DoctorConsultations />
+          </ProtectedRoute>
+        }
+      /> 
 
-            <Route
-            path="/patient/payment/:appointmentId"
-            element={
-                <ProtectedRoute allowedRoles={["patient"]}>
-                    <Payment />
-                </ProtectedRoute>
-            }
-            />
+       <Route
+        path="/patient/timeline"
+        element={
+          <ProtectedRoute allowedRoles={['patient']}>
+            <PatientTimeline />
+          </ProtectedRoute>
+        }
+      />
 
-            <Route
-            path="/doctor/consultations"
-            element={
-                <ProtectedRoute allowedRoles={["doctor"]}>
-                    <DoctorConsultations />
-                </ProtectedRoute>
-            }
-            />
+      <Route
+        path="/admin/clinical-records"
+        element={
+          <ProtectedRoute allowedRoles={['admin']}>
+            <AdminClinicalRecords />
+          </ProtectedRoute>
+        }
+      /> 
 
-            <Route
-            path="/patient/timeline"
-            element={
-                <ProtectedRoute allowedRoles={["patient"]}>
-                    <PatientTimeline />
-                </ProtectedRoute>
-            }
-            />
-
-            
-            <Route
-            path="/admin/clinical-records"
-            element={
-                <ProtectedRoute allowedRoles={["admin"]}>
-                    <AdminClinicalRecords />
-                </ProtectedRoute>
-            }
-            />
-
-        </Routes>
-        
-    )
+      <Route
+        path="/doctor/patient/:patientId"
+        element={<DoctorPatientProfile/>}
+        />
+    </Routes>
+  );
 }
--- apps/web-app/src/app/router/ProtectedRoute.tsx
+++ apps/web-app/src/app/router/ProtectedRoute.tsx
@@ -1,24 +1,23 @@
-import { Navigate } from "react-router-dom";
-import { useAuthStore } from "@/store/auth/auth.store";
-import type { JSX } from "react";
+import { Navigate } from 'react-router-dom';
+import { useAuthStore } from '@/store/auth/auth.store';
+import type { JSX } from 'react';
 
-interface ProtectedRoutesProps{
-    children:JSX.Element,
-    allowedRoles?:string[]
+interface ProtectedRoutesProps {
+  children: JSX.Element;
+  allowedRoles?: string[];
 }
 
 export default function ProtectedRoute({
-    children,
-    allowedRoles,
-}:ProtectedRoutesProps){
+  children,
+  allowedRoles,
+}: ProtectedRoutesProps) {
+  const user = useAuthStore((s) => s.user);
 
-    const user=useAuthStore((s)=>s.user);
-
-    if(!user){
-        return <Navigate to="/login" replace/>;
-    }
-    if(allowedRoles && !allowedRoles.includes(user.role)){
-        return<Navigate to="/login" replace/>;
-    }
-    return children;
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
 }
