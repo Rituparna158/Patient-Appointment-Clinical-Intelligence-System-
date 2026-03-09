c5951dec2c30b84175795e32626395c0e7f63e45
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-03-09 03:29:21 -0700

    chore: format code with prettier

--- apps/web-app/src/store/auth/auth.types.ts
+++ apps/web-app/src/store/auth/auth.types.ts
@@ -1,12 +1,12 @@
-import type {User} from "@/types/auth.types";
+import type { User } from '@/types/auth.types';
 
-interface AuthState{
-    user: User | null;
-    token:string | null
+interface AuthState {
+  user: User | null;
+  token: string | null;
 }
 
-interface AuthActions{
-    login:(user:User)=>void;
-    logout:()=>void;
+interface AuthActions {
+  login: (user: User) => void;
+  logout: () => void;
 }
-export type {AuthActions,AuthState}
+export type { AuthActions, AuthState };
