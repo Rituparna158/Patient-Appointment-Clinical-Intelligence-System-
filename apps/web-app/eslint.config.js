9b3a2d9bb9d09fc988a4fc2d2c86f9fcaec3d660
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-03-19 18:19:19 +0530

    fix(ESLint): fix Eslint setup for frontend by adding rules

--- apps/web-app/eslint.config.js
+++ apps/web-app/eslint.config.js
@@ -21,3 +21,51 @@
 //     },
 //   },
 // ])
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactRefresh.configs.vite,
+
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+  },
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+  },
+
+  {
+    files: ['src/components/ui/**/*.{ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
+
+  {
+    files: ['tailwind.config.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      'no-undef': 'off',
+    },
+  },
+]);
