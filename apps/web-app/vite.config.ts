22a0e9221472cb890f6b9dcf5ee6c9a0d5762032
Rituparna Rath <rituparnar@mindfiresolutions.com>
2026-03-23 12:07:26 +0530

    refactor: to analyze the bundle size

--- apps/web-app/vite.config.ts
+++ apps/web-app/vite.config.ts
@@ -3,18 +3,20 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig(() => {
-  const isAnalyze = process.env.ANALYZE === 'true';
-  const shouldOpen = process.env.ANALYZE_OPEN === 'true';
+export default defineConfig(({ mode }) => {
+  const isAnalyze = mode === 'analyze';
+
+  console.log('MODE =', mode, 'isAnalyze =', isAnalyze);
 
   return {
     plugins: [
       react(),
+
       ...(isAnalyze
         ? [
             visualizer({
-              filename: 'stats.html',
-              open: shouldOpen,
+              filename: './dist/stats.html',
+              open: true,
               gzipSize: true,
               brotliSize: true,
             }),
@@ -36,6 +38,7 @@ export default defineConfig(() => {
 
     build: {
       sourcemap: false,
+
       rollupOptions: {
         output: {
           manualChunks: {
